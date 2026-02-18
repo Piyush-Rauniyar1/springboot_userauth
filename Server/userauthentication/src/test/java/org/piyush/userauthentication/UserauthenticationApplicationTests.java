84ecb882cc13d4656bcaecee1c0a71506ad66669
RAUNIfied <Piyushrauniyar16@gmail.com>
2026-02-18 23:38:18 +0545

    Initial project setup
    - Initialized Spring Boot backend
    - Setup React frontend
    - Installed and configured Axios API service layer
    
    Closes #1, #9, #10

---
+++ Server/userauthentication/src/test/java/org/piyush/userauthentication/UserauthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.piyush.userauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserauthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
